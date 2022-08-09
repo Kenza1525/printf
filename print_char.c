7139d5f0ddae406192080f6b3c232f853e8206bf
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-08-09 08:12:40 -0700

    printf_char.c

---
+++ print_char.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * printf_char - prints a char.
+ * @val: arguments.
+ * Return: 1.
+ */
+int print_char(va_list ap)
+{
+	char s;
+
+	s = va_arg(ap, int);
+	_putchar(s);
+	return (1);
+}
