3624a2af32eaa60fb962bf821ed4e9887cf75a9a
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-08-09 07:14:54 -0700

    _putchar

---
+++ _putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the charac to stdout
+ *
+ * @c: The character to be printed.
+ *
+ * Return: Always 1 on success
+ *
+ * on error, -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return (_putchar(1, &c, 1));
+}
